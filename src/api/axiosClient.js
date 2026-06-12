6dfbabaf177847501d4a1aa61427f54342c27471
finlab-123 <tech.dev@finlab.one>
2026-06-12 10:21:46 +0530

    fix: resolve loop rendering objects and casing strings

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update this to your backend server URL if different
+  baseURL: 'https://bynd-backend-owi6.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
